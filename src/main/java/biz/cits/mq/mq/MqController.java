88a19cea0cda2ba5c4e14e720487dcc70fa55b88
Hardik <hardikgw@gmail.com>
2019-10-15 12:46:35 -0400

    cp

--- src/main/java/biz/cits/mq/mq/MqController.java
+++ src/main/java/biz/cits/mq/mq/MqController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.JMSProducer;
 import java.util.ArrayList;
 import java.util.Map;
 
